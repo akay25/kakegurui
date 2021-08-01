18cfbbfb54e9e8f691ea48b37713abbbd1cd5861
Ajay M <ajay@shivy.co.in>
2021-08-01 14:06:29 +0530

    Using axios configuration

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: "http://localhost:3000/v1"
+  baseURL: process.env.VUE_APP_API_ENPOINT
 });
 
 // Alter defaults after instance has been created
