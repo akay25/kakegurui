9e3be68fbec5f2f9496f92b0d6ea8849dd7154b2
Ajay M <ajay@shivy.co.in>
2021-08-10 01:09:40 +0530

    Separated card component

--- frontend/src/pages/room/index.vue
+++ frontend/src/pages/room/index.vue
@@ -16,62 +16,8 @@ import _ from "lodash";
 import { mapGetters, mapMutations } from "vuex";
 import Board from "@/components/Board.vue";
 import axios from "@/api";
+import { cards } from "@/data/cards";
 import { clearLocalStorage } from "@/utils";
-
-const cards = [
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/011.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/013.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/014.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/015.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/017.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/018.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/019.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/020.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/021.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/022.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/023.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/024.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/027.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/028.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/029.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/030.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/031.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/032.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/033.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/034.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/035.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/036.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/037.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/038.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/040.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/041.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/042.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/043.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/044.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/045.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/046.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/047.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/048.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/049.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/050.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/051.png" },
-  { image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/052.png" }
-];
 
 const CARDS = _.shuffle(_.concat(cards, cards));
 
