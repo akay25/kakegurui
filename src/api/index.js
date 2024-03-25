777b10efd132286a3e4f262d78244199cfa6d958
Ajay M <ajay@shivy.co.in>
2024-03-25 14:00:43 +0530

    Fixed minor typo

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_ENPOINT
+    baseURL: process.env.VUE_APP_API_ENDPOINT
 });
 
 export default instance;
